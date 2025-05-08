f33b8a38a735d9dabfeab5830dead33a20fd3d0b
Osopardo1226 <julianchin1226@gmail.com>
2025-05-08 15:24:47 -0500

    Navbar de Admin actualizada y inicio de proceso de la nueva sidebar

---
+++ src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SidebarAdmin() {
+    return (
+        <div className="sidebar-admin">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+export default SidebarAdmin;
