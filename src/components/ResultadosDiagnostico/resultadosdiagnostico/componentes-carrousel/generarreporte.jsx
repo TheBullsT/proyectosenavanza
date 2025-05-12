75df11139a2632a305e0d8385600a5e0eb6d20b5
Osopardo1226 <julianchin1226@gmail.com>
2025-05-12 13:56:06 -0500

    reporte carousell

--- src/components/ResultadosDiagnostico/resultadosdiagnostico/componentes-carrousel/generarreporte.jsx
+++ src/components/ResultadosDiagnostico/resultadosdiagnostico/componentes-carrousel/generarreporte.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import './generarreporte.css';
+import reporte from '../../../../assets/img/img-resultados-diagnostico/reporte.png';
+
 
 function GenerarReporte(){
     return(
-        <div className="container">
+        <div className="container-reporte">
             <div className="titulo-reporte">
-                <h1>!Conoce el programa de formacion 
-                mas recomendadopara ti¡</h1>
+                <h1>¿Quieres tener tus resultados 
+                    <br /> 
+                del diagnóstico?</h1>
+            </div>
+            <div className="generar-reporte">
+                <img src={reporte} alt="Imagen reporte" />
+                <h2>Genera un reporte del resultado del diagnóstico para que puedas verlos con más calma</h2>
+                <button className="boton-generar-reporte">Generar Reporte</button>
             </div>
         </div>
     );
