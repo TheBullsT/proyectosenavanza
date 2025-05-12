d6e4581e1c2c656be4c367d14f9070f819dce4f6
TheBullst <jmontillavanegas@gmail.com>
2025-05-12 17:23:41 -0500

    pop up

--- src/components/DiagnosticoEmpresarial/Formulario.jsx
+++ src/components/DiagnosticoEmpresarial/Formulario.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Formulario.css';
 import BannerHelp from './BannerHelp';
 import arrow from '../../assets/img/arrow-green.png'; 
 import person from '../../assets/img/person-form.png';
+import Popup from './PopUp'; 
 
-const FormsLayout = () => {
-    return (
+
+function FormsLayout() {
+  const [mostrarPopup, setMostrarPopup] = useState(false);  // Estado local del popup
+
+  const abrirPopup = () => {
+    console.log("Abrir popup llamado");
+    setMostrarPopup(true);  // Cambiar el estado para mostrar el popup
+  };
+
+  const cerrarPopup = () => setMostrarPopup(false);
+   return (
         <div className="forms-layout">
             <div className="title"><h1>Formulario</h1></div>
             <div className="barra2">
@@ -20,13 +30,14 @@ const FormsLayout = () => {
             <BannerHelp />
             <div className='main-section'>
                 <span className='information'>
-                    ¡Presiona este botón <br />para conocer qué <br /> programas de formación de <br /> formación son <br /> perfectos para usted!
+                    ¡Presiona este botón <br />para conocer qué <br /> programas de <br /> formación son <br /> perfectos para usted!
                 </span>
             </div>
             <div className='press'>
                 <div className='press-boton'>
                     <img className='imagen-flecha' src={arrow} alt="Imagen de flecha" />
-                    <button className="diagnostico-button">Diagnóstico</button>
+                <button className="diagnostico-button" type='button' onClick={abrirPopup}>Diagnostico</button>
+                {mostrarPopup && <Popup cerrar={cerrarPopup} />}
                 </div>
                 <img className='person-image' src={person} alt="Persona formulario" />
             </div>
@@ -35,3 +46,4 @@ const FormsLayout = () => {
 }
 
 export default FormsLayout;
+
