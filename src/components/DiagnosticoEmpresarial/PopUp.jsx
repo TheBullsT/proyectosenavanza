a2d0da997aeeb9169f7b1ffce8ac1019cb5a28a6
TheBullst <jmontillavanegas@gmail.com>
2025-05-14 10:21:27 -0500

    pop up actualizacion

--- src/components/DiagnosticoEmpresarial/PopUp.jsx
+++ src/components/DiagnosticoEmpresarial/PopUp.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import exito from '../../assets/img/img-resultados-diagnostico/exito.png'
 import "./Popup.css";
 
 function Popup({ cerrar }) {
   return (
     <div className="popup-overlay">
       <div className="popup">
-        <h2>Este es el popup</h2>
-        <p>Contenido del popup aquí.</p>
-        <button onClick={cerrar}>Cerrar</button>
+        <div className='diagnostico-completo'>
+          <h2>!Tu diagnostico <br />ha sido  completado con éxito¡</h2>
+          <img src={exito} alt="Imagen de exito" />
+          <p>Descarga tu reporte y <br />revisa los resultados al detalle</p>
+        </div>
+        <div className='botones-diagnostico'>
+          <button className='resultados' onClick={cerrar}>Resultados</button>
+          <button className='descargar' onClick={cerrar}>Descargar</button>
+        </div>
       </div>
     </div>
   );
