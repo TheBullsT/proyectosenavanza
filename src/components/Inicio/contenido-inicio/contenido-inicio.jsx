dcaefd64f4cd6c15e49028b8a74f705c51e14dab
TheBullst <jmontillavanegas@gmail.com>
2025-05-20 14:36:20 -0500

    contenido inicio

---
+++ src/components/Inicio/contenido-inicio/contenido-inicio.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import image from '../../../assets/img/img-inicio/imagen-inicio.png'
+import computador from '../../../assets/img/img-inicio/material-symbols_computer-rounded.png'
+import flechita from '../../../assets/img/img-inicio/ion_arrow-redo.png'
+import './contenido-inicio.css';
+
+function ContenidoInicio(){
+    return(
+        <div className="contenido-inicio">
+            <div className="titulo-inicio">
+                <img src={image} alt="Imagen de inicio" />
+                <p>¿INTERESADO EN <br /> NUESTROS PROGRAMAS DE <br /> FORMACION?</p>
+            </div>
+            <div className="programas-inicio">
+                <div className="programa-inicio">
+                    <div className="imagen-programa">
+                        <img src={computador} alt="Computador" />
+                    </div>
+                    <div className="nombre-programa"><p>Programa de Formación</p></div>
+                    <div className="descripcion-programa">Descripción corta del programa <br /> de Formación</div>
+                    <div className="botones-inicio">
+                        <img src={flechita} alt="Flecha" />
+                        <button className="boton-inicio">¡PRESIONA AQUI!</button>
+                    </div>
+                </div>
+                <div className="programa-inicio">
+                    <div className="imagen-programa">
+                        <img src={computador} alt="Computador" />
+                    </div>
+                    <div className="nombre-programa"><p>Programa de Formación</p></div>
+                    <div className="descripcion-programa">Descripción corta del programa <br /> de Formación</div>
+                    <div className="botones-inicio">
+                        <img src={flechita} alt="Flecha" />
+                        <button className="boton-inicio">¡PRESIONA AQUI!</button>
+                    </div>
+                </div>
+                <div className="programa-inicio">
+                    <div className="imagen-programa">
+                        <img src={computador} alt="Computador" />
+                    </div>
+                    <div className="nombre-programa"><p>Programa de Formación</p></div>
+                    <div className="descripcion-programa">Descripción corta del programa <br /> de Formación</div>
+                    <div className="botones-inicio">
+                        <img src={flechita} alt="Flecha" />
+                        <button className="boton-inicio">¡PRESIONA AQUI!</button>
+                    </div>
+                </div>
+                <div className="programa-inicio">
+                    <div className="imagen-programa">
+                        <img src={computador} alt="Computador" />
+                    </div>
+                    <div className="nombre-programa"><p>Programa de Formación</p></div>
+                    <div className="descripcion-programa">Descripción corta del programa <br /> de Formación</div>
+                    <div className="botones-inicio">
+                        <img src={flechita} alt="Flecha" />
+                        <button className="boton-inicio">¡PRESIONA AQUI!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ContenidoInicio
