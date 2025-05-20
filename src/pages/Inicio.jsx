8f0616bba6a98bea933297d9b04b2e48023982b9
TheBullst <jmontillavanegas@gmail.com>
2025-05-20 15:08:54 -0500

    contenido inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,7 @@
 import NavBarInicio from "../components/Inicio/Navbar_inicio";
 import Footer from "../components/Home/Footer/footer";
 import Fondo from "../assets/img/inicio/fondo_completo.png"
+import ContenidoInicio from "../components/Inicio/contenido-inicio/contenido-inicio";
 
 const Inicio = () => {
     return (
@@ -9,6 +10,7 @@ const Inicio = () => {
             <div className="fondillo">
                 <img src={Fondo} alt="" />
             </div>
+            <ContenidoInicio/>
             <Footer/>       
         </div>
         
