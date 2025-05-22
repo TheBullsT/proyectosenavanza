ac645bbaf0a72018b70dd3b5930484978f6ec4f4
TheBullst <jmontillavanegas@gmail.com>
2025-05-22 13:03:18 -0500

    vite integrado

--- src/components/Loading/loading.jsx
+++ src/components/Loading/loading.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './loading.css';
+import logo from '../../assets/img/Logo_SENAVANZA.jpg'; 
 
-const Loading = () => {
+const Loading = ({ mensaje = "Cargando SENAVANZA..." }) => {
   return (
     <div className="loading-container">
+      {logo && <img src={logo} alt="Logo SENAVANZA" className="loading-logo" />}
       <div className="spinner"></div>
-      <p className="loading-text">Cargando SENAVANZA...</p>
+      <p className="loading-text">{mensaje}</p>
     </div>
   );
 };
