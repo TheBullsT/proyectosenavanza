51d46ff562c973a0c2ef41458ae600062d7257c8
Osopardo1226 <julianchin1226@gmail.com>
2025-05-22 15:29:05 -0500

    Admin bases

--- src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
+++ src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
@@ -5,7 +5,11 @@ function SidebarAdmin() {
     return (
         <div className="sidebar-admin">
             <ul>
-                <li></li>
+                <li>Prueba1</li>
+                <li>Prueba2</li>
+                <li>Prueba3</li>
+                <li>Prueba4</li>
+                <li>Prueba5</li>
             </ul>
         </div>
     );
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,11 +1,12 @@
 import NavFooterAdmin from '../layouts/LayoutAdmin';
+import SidebarAdmin from '../components/Admin/SidebarAdmin/SidebarAdmin';
 import React from 'react';
 
 const AdminHome = () => {
     return (
         
       <NavFooterAdmin>
-        <h1>Esto si o si va a funcionar</h1>
+        <SidebarAdmin/>
       </NavFooterAdmin>
     );
   };
