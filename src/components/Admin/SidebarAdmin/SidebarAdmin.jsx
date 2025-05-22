263293a651527f571c87d4b9447a7c48f0da7e1f
Osopardo1226 <julianchin1226@gmail.com>
2025-05-22 18:09:30 -0500

    Organizando bases de vistas de admin

--- src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
+++ src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import "./SidebarAdmin.css";
+import { CgProfile } from "react-icons/cg";
 
 
 function SidebarAdmin() {
     return (
         <div className="sidebar-admin">
-            <ul>
-                <li>Prueba1</li>
-                <li>Prueba2</li>
-                <li>Prueba3</li>
-                <li>Prueba4</li>
-                <li>Prueba5</li>
-            </ul>
+            <div className="bar">
+                <div className="perfil-admin">
+                    <CgProfile className="perfil-icon" />
+                </div>
+                <div className="central">
+                    <div className="home-admin">
+                        
+                    </div>
+                    <div className="programas-admin">
+                        
+                    </div>
+                    <div className="empresa-admin">
+                        
+                    </div>
+                    <div className="usuario-admin">
+                        
+                    </div>
+                    <div className="reporte-admin">
+                        
+                    </div>
+                </div>
+                <div className="logout-admin">
+                    
+                </div>
+            </div>
         </div>
     );
 }
--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -1,12 +1,10 @@
 import NavFooterAdmin from '../layouts/LayoutAdmin';
-import SidebarAdmin from '../components/Admin/SidebarAdmin/SidebarAdmin';
 import React from 'react';
 
 const AdminHome = () => {
     return (
         
       <NavFooterAdmin>
-        <SidebarAdmin/>
       </NavFooterAdmin>
     );
   };
