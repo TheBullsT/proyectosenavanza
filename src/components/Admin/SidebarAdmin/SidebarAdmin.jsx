4918181bc032930a80520cf411f792c5fc03d9ba
TheBullst <jmontillavanegas@gmail.com>
2025-05-27 15:53:42 -0500

    arreglado el link to

--- src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
+++ src/components/Admin/SidebarAdmin/SidebarAdmin.jsx
@@ -2,30 +2,27 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SidebarAdmin.css";
 import { FaHome } from "react-icons/fa";
-import { MdHomeRepairService } from "react-icons/md";
-import { MdSchool } from "react-icons/md";
+import { MdHomeRepairService, MdSchool } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
 
-
 function SidebarAdmin() {
-
-    const [ActiveMenu, SetMenuOn] = useState(false)
+    const [ActiveMenu, SetMenuOn] = useState(false);
 
     const abrirMenu = (menuOn) => {
         SetMenuOn(ActiveMenu === menuOn ? null : menuOn);
-    }
+    };
 
     return (
         <div className="sidebar-admin">
-
             <ul className="ulContainer">
                 <h4 className="menu">Menu</h4>
 
-                <li className="liContainer">
-                    <FaHome className="sidebaricon" />
-                    <Link to="/adminhome" className="no-estilo"><p className="itemNames">Home</p></Link>
-                </li>
-
+                <Link to="/adminhome" className="no-estilo">
+                    <li className="liContainer">
+                        <FaHome className="sidebaricon" />
+                        <p className="itemNames">Home</p>
+                    </li>
+                </Link>
 
                 <li className="liContainer" onClick={() => abrirMenu("empresas")}>
                     <MdHomeRepairService className="sidebaricon" />
@@ -68,13 +65,13 @@ function SidebarAdmin() {
                         <li>Estado de Usuarios</li>
                     </ul>
                 )}
-
             </ul>
 
-            <Link to='/inicio' className="ButtonCerrarSesionAdmin">
-            Cerrar Sesión
+            <Link to='/inicio' className="no-estilo">
+                <div className="ButtonCerrarSesionAdmin">
+                    Cerrar Sesión
+                </div>
             </Link>
-
         </div>
     );
 }
