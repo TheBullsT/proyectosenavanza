6a68f9801aece711f507175d456b13750234f8a2
Osopardo1226 <julianchin1226@gmail.com>
2025-05-29 14:57:04 -0500

    Diagnostico Comentado

--- src/components/DiagnosticoEmpresarial/BannerHelp.jsx
+++ src/components/DiagnosticoEmpresarial/BannerHelp.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import './Formulario.css';
-import { BiTask } from "react-icons/bi";
+import './Formulario.css'; // Importamos el archivo CSS con estilos globales y específicos
+import { BiTask } from "react-icons/bi"; // Importamos el ícono BiTask de react-icons
 
 const BannerHelp = () => {
     return (
+        // Contenedor principal del banner con borde y fondo suave
         <div className='container-banner'>
+
+            {/* Contenedor para el ícono a la izquierda */}
             <div className='icon-banner'>
+                {/* Ícono que representa la tarea o diagnóstico */}
                 <BiTask className='icon-banner-diagnostico' />
             </div>
+
+            {/* Contenedor para el texto informativo */}
             <div className='banner-info'>
+                {/* Título en negrita que explica el propósito del banner */}
                 <p><b>¿Qué beneficios trae el diagnóstico empresarial?</b></p>
+
+                {/* Lista con los puntos clave de los beneficios */}
                 <ul>
                     <li>Identificas áreas de mejora.</li>
                     <li>Encuentras Programas de Formación adecuados.</li>
@@ -20,3 +29,4 @@ const BannerHelp = () => {
 }
 
 export default BannerHelp;
+
