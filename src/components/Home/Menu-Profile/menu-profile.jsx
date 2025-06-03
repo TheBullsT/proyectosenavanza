0cac0d85cc950af6482e95a7a3df2e3d5dac6412
TheBullst <jmontillavanegas@gmail.com>
2025-06-03 14:14:53 -0500

    arreglado

--- src/components/Home/Menu-Profile/menu-profile.jsx
+++ src/components/Home/Menu-Profile/menu-profile.jsx
@@ -29,39 +29,39 @@ const MenuProfile = forwardRef(function MenuProfileComponent(props, ref) {
             {/* {cerrandoSesion ? (
                 <CerrarSesion />
             ) : ( */}
-                <div ref={ref} className="dropdown-menu">
-                    {/* Encabezado de la sección "Información de perfil" */}
-                    <div className="section-header">
-                        <div className="subtitulo">Información de perfil</div>
-                        <div className="linea"></div>
-                    </div>
-
-                    {/* Lista de enlaces relacionados al perfil */}
-                    <ul className="menu-list">
-                        <li><Link to="/perfil">Perfil</Link></li>
-                        <li><Link to="/editarperfil">Editar perfil</Link></li>
-                    </ul>
-
-                    {/* Encabezado de la sección "Opciones de empresa" */}
-                    <div className="section-header">
-                        <div className="subtitulo">Opciones de empresa</div>
-                        <div className="linea"></div>
-                    </div>
-
-                    {/* Lista de enlaces relacionados a la empresa */}
-                    <ul className="menu-list">
-                        <li><Link to="/diagnostico-empresarial">Diagnóstico Empresarial</Link></li>
-                        <li><Link to="/resultado-diagnostico">Resultados de Diagnóstico</Link></li>
-                    </ul>
-
-                    {/* Línea separadora final */}
+            <div ref={ref} className="dropdown-menu">
+                {/* Encabezado de la sección "Información de perfil" */}
+                <div className="section-header">
+                    <div className="subtitulo">Información de perfil</div>
                     <div className="linea"></div>
-
-                    {/* Enlace para cerrar sesión */}
-                    <ul className="menu-list">
-                        <li onClick={handleCerrarSesion}>Cerrar Sesión</li>
-                    </ul>
                 </div>
+
+                {/* Lista de enlaces relacionados al perfil */}
+                <ul className="menu-list">
+                    <li><Link to="/perfil">Perfil</Link></li>
+                    <li><Link to="/editarperfil">Editar perfil</Link></li>
+                </ul>
+
+                {/* Encabezado de la sección "Opciones de empresa" */}
+                <div className="section-header">
+                    <div className="subtitulo">Opciones de empresa</div>
+                    <div className="linea"></div>
+                </div>
+
+                {/* Lista de enlaces relacionados a la empresa */}
+                <ul className="menu-list">
+                    <li><Link to="/diagnostico-empresarial">Diagnóstico Empresarial</Link></li>
+                    <li><Link to="/resultadosdediagnostico">Resultados de Diagnóstico</Link></li>
+                </ul>
+
+                {/* Línea separadora final */}
+                <div className="linea"></div>
+
+                {/* Enlace para cerrar sesión */}
+                <ul className="menu-list">
+                    <li>Cerrar Sesión</li>
+                </ul>
+            </div>
         </>
     );
 });
