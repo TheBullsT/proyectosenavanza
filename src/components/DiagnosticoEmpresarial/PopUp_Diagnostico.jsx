09247a36c2fb5b15c4f232632865a9d4ca1b7af3
elquicenin <quicenodavid29@gmail.com>
2025-07-03 18:28:53 +0500

    PopUps Terminados

--- src/components/DiagnosticoEmpresarial/PopUp_Diagnostico.jsx
+++ src/components/DiagnosticoEmpresarial/PopUp_Diagnostico.jsx
@@ -7,31 +7,33 @@ import './Popup.css'; // Asumiendo que tienes estilos separados
 function Popup_Diagnostico({ cerrar, enviar }) {
     return (
         <div className="overlay">
-            <div className="popup2">
-                <h2 className="popup-title">Diagnostico Empresarial</h2>
-                <div className="underline" />
+            <form action="" className='overlay'>
+                <div className="popup2">
+                    <h2 className="popup-title">Diagnostico Empresarial</h2>
+                    <div className="underline" />
 
 
-                <div className="form-group">
-                    <input type="text" placeholder="Área requerida" className="input-box" />
+                    <div className="form-group">
+                        <input type="text" placeholder="Área requerida" className="input-box" />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="text" placeholder="Describa brevemente las ocupaciones para el aprendiz..." className="input-box2" />
+                    </div>
+
+                    <button className="submit-btn" onClick={enviar}>
+                        <FaCheck className='icon-check' />Enviar diagnóstico
+                    </button>
+                    <div>
+                        <button className="close-btn" onClick={cerrar}>
+                            <MdCancel /> Cerrar
+                        </button>
+                    </div>
+
                 </div>
-
-                <div className="form-group">
-                    <input type="text" placeholder="Describa brevemente las ocupaciones para el aprendiz..." className="input-box2" />
-                </div>
-
-                <button className="submit-btn" onClick={enviar}>
-                    <FaCheck/>Enviar diagnóstico
-                </button>
-                <div>
-                  <button className="close-btn" onClick={cerrar}>
-                      <MdCancel/> Cerrar
-                  </button>
-                  </div>
-                
-            </div>
+            </form>
         </div>
     );
 }
 
-export default Popup_Diagnostico;
+export default Popup_Diagnostico; 
