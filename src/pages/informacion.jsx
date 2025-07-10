41201d099619bf9f9437a19d6c61c537c33b4b46
TheBullst <jmontillavanegas@gmail.com>
2025-07-10 10:08:49 -0500

    informacion gov

--- src/pages/informacion.jsx
+++ src/pages/informacion.jsx
@@ -2,10 +2,12 @@ import NavBarInicio from "../components/Inicio/Navbar_inicio"; // Barra de naveg
 import Footer from "../components/Home/Footer/footer"; // Pie de página común
 import HeroInfo from "../components/Informacion/hero"; // Sección principal tipo "hero" con información destacada
 import Contenido from "../components/Informacion/contenido"; // Contenido principal de la página de información
+import Gov from '../components/BarraGov/gov';
 
 const Informacion = () => {
     return (
         <div>
+            <div><Gov/></div>
             {/* Renderiza la barra de navegación */}
             <NavBarInicio />
             {/* Sección destacada tipo hero */}
